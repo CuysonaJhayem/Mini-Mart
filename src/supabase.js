cdb5eed283fcd0afa8fce44694a329379a1cb8a8
CuysonaJhayem <cuysonamj12@gmail.com>
2026-06-01 08:17:21 +0800

    Delete src directory

--- src/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// 👇 I-replace ni ang imong actual Supabase credentials
-// Makita nimo ni sa: Supabase Dashboard → Project Settings → API
-const SUPABASE_URL = 'https://ezhqqutzsqrxaqtzynso.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_e6SOLzz5xPBUEdREtxI1ow__hQgexAi'; // ang "anon public" key
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
